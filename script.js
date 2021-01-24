474fbf3f7e91e9d4959d9acfc0e5860af8d0a14f
omer rokach <omerrokach@gmail.com>
2021-01-24 13:06:16 +0200

    appending all the listRow to the main list

--- script.js
+++ script.js
@@ -2,21 +2,22 @@ const list = document.querySelector(`ul`);
 const input = document.querySelector(`input`);
 const addButton = document.querySelector(`button`);
 
-addButton.onclick = function(){
+addButton.onclick = function() {
     let item = input.value;
     input.value = '';
 
-    const liItem = document.createElement(`li`);
-    const spanText = document.createElement(`span`);
+    const listRow = document.createElement(`li`);
+    const listSpan = document.createElement(`span`);
     const delButton = document.createElement(`button`);
 
-    liItem.appendChild(spanText);
-    spanText.textContent(item);
-    liItem.appendChild(delButton);
-    delButton.textContent('delete');
+    listRow.appendChild(listSpan);
+    listSpan.textContent = item;
+    listRow.appendChild(delButton);
+    delButton.textContent = 'delete';
+    list.appendChild(listRow)
 
     delButton.onclick = function(){
-        list.remove(listItem);
+        list.remove(listRow);
     }
 
 }
