79b0be8daef708a5ecdab14c8c7aabad617b7928
omer rokach <omerrokach@gmail.com>
2021-01-24 12:52:26 +0200

    creating the func and storying the input val to my var and clearing the input val

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const ul = document.getElementsByTagName(ul);
-const input = document.getElementsByTagName(input);
-const button = document.getElementsByTagName(button);
+const list = document.querySelector(`ul`);
+const input = document.querySelector(`input`);
+const addButton = document.querySelector(`button`);
+
+addButton.onclick = function(){
+    let item = input.value;
+    input.value = '';
+
+    
+
+}
